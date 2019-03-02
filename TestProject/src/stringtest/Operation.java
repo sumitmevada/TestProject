865c3c85b0bae946d017de2dcb2e33adc9789fb5
sumitmevada <itexpertnet18@gmail.com>
2019-03-02 19:13:53 +0530

    get First and Last Character from String

--- TestProject/src/stringtest/Operation.java
+++ TestProject/src/stringtest/Operation.java
@@ -2,4 +2,25 @@ package stringtest;
 
 public class Operation {
 
+	private String message;
+
+	  public Operation(String message) {
+	    this.message=message;
+	  }
+
+	  public char getFirst(){
+	    return message.charAt(0);
+	  }
+
+	  public char getLast(){
+	    int len=message.length();
+	    return message.charAt(len-1);
+	  }
+
+	  public static void main(String[] args) {
+	    Operation fl=new Operation("Here you go");
+	    System.out.println("First Char is "+fl.getFirst());
+	    System.out.println("First Char is "+fl.getLast());
+	  }
+	
 }
